0e57a92e28e17d6c4c0189a9568d7d7c2259ce5d
Krishna Chaitanya Tripuraneni <krishnachaitanyatripuraneni@Krishnas-MacBook-Air.local>
2023-10-01 11:44:57 +0530

    install next js and create the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head(){
+    return(
+        <>
+            <title>Google Clone Next js13</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <meta name="description" content="Google clone created by Next js13"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </>
+    )
+}
